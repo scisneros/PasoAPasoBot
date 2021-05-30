ef8918b0eaeadd0eef22cc86a632fe894ada2a0e
scisneros <sebastian.cisneros384@gmail.com>
2021-05-30 04:10:46 -0400

    Initial data loading

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ import data
 from config.logger import logger
 
 
-def fetch_data(context):
+def fetch_data():
     url = "https://e.infogram.com/81277d3a-5813-46f7-a270-79d1768a70b2"
     logger.info("Consultando data de pasos de comunas...")
     try:
@@ -20,10 +20,19 @@ def fetch_data(context):
     scraped_data = [line for line in response.content.decode().split('\n') if "window.infographicData" in line]
     scraped_data_json = json.loads(scraped_data[0].replace("<script>window.infographicData=", "", 1).replace(";</script>", "", 1))
     infographic_data = scraped_data_json["elements"]["content"]["content"]["entities"]["3f026fbf-998f-4ae2-852b-94fa3a2f71d4"]["props"]["chartData"]["data"][0][1:]
-    data.new_data = infographic_data
-    # check diff
+    return infographic_data
+
+
+def check_for_changes(context):
+    data.new_data = fetch_data()
+
+    # check diff here
+
     data.current_data = data.new_data
     
-    # Dump de la data a archivo
-    with open(path.relpath('data/infographic_data'), "w") as infographic_data_file:
-        json.dump(infographic_data, infographic_data_file, indent=4)
+    save_data()
+
+
+def save_data():
+    with open(path.relpath('data/data_comunas'), "w") as infographic_data_file:
+        json.dump(data.current_data, infographic_data_file, indent=4)
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+import json
+
+from os import path
+
 from telegram.ext import CommandHandler, Filters
 
+import data
 import constants
+
 from bot import updater, dp, jq
 from commands import start, comuna, get_log, force_check
 from config.auth import admin_ids
-from functions import fetch_data
+from config.logger import logger
+from functions import fetch_data, check_for_changes, save_data
 
 
 def main():
-    jq.run_repeating(fetch_data, interval=300, first=(1 if constants.CHECK_FIRST else None), name="job_fetch")
+    try:
+        with open(path.relpath("data/data_comunas", "r")) as datajsonfile:
+            data.current_data = json.load(datajsonfile)
+        logger.info("Data cargada desde disco, se hará un chequeo de cambios inicial")
+        check_first = True
+    except OSError:
+        logger.info("No se encontró data en disco, fetch inicial se hará sin chequear cambios.")
+        check_first = False
+        data.current_data = fetch_data()
+        save_data()
+
+    jq.run_repeating(check_for_changes, interval=300, first=(1 if check_first else None), name="job_fetch")
 
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('comuna', comuna))
