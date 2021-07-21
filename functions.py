285e0e84e094a946e8e273b3f74cf4c9f8e451e3
scisneros <sebastian.cisneros384@gmail.com>
2021-07-21 03:26:14 -0400

    Change notifications format

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 from constants import CHANGE_DAY, PASOS_NAMES, PASOS_EMOJIS
 import json
 from os import path
-from utils import searchBySlug, send_long_message, slugify
+from utils import searchBySlug, send_long_message, slugify, try_msg
 
 import copy
 import requests
@@ -51,6 +51,8 @@ def check_for_changes(context):
     regiones_changes = []
     up_count = 0
     down_count = 0
+    enter_counts = [0, 0, 0, 0]
+    leave_counts = [0, 0, 0, 0]
     for old_region in data.current_data["regiones"]:
         new_region = searchBySlug(old_region["slug"], data.new_data["regiones"])
         comunas_changes = []
@@ -66,44 +68,67 @@ def check_for_changes(context):
                     up_count += 1
                 else:
                     down_count += 1
+                    
+                enter_counts[int(new_comuna["paso"])-1] += 1
+                leave_counts[int(old_comuna["paso"])-1] += 1
         if comunas_changes:
             regiones_changes.append({"nombre": new_region["nombre"], "comunas": comunas_changes})
     
     if regiones_changes:
-        notify_changes(context.bot, regiones_changes, up_count, down_count)
+        notify_changes(context.bot, regiones_changes, {"up": up_count, "down": down_count, "enter": enter_counts, "leave": leave_counts})
     
     data.current_data = data.new_data
 
     save_data()
 
 
-def notify_changes(bot, changes, up_count, down_count):
+def notify_changes(bot, changes, counts):
+    time_now = datetime.now().strftime('%A %d/%m/%y %H:%M').capitalize()
+
+    new_counts = get_steps_counts(current=False)
     message = f"<b>[Cambios detectados]</b>\n"
-    message += f"Avances: {up_count} - Retrocesos: {down_count}\n"
-    message += f"<i>{datetime.now().strftime('%A %d/%m/%y %H:%M').capitalize()}</i>\n\n"
+    message += f"<i>{time_now}</i>\n"
+    message += "\n"
+    message += f"<b>Avances:</b> {counts['up']}\n"
+    message += f"<b>Retrocesos:</b> {counts['down']}\n"
+    message += "\n"
+    message += "<b>Comunas por paso:</b>\n"
+    for i in range(len(new_counts)):
+        message += f"{PASOS_EMOJIS[i+1]} <b>Paso {i+1}</b>: {new_counts[i]} [+{counts['enter'][i]}, -{counts['leave'][i]}]\n"
+    message += "\n"
+    #current_day = int(datetime.now().strftime('%w'))
+    current_day = 1
+    if current_day in CHANGE_DAY:
+        message += f"<b>Avances</b> vigentes desde el <b>{CHANGE_DAY[current_day]['up']['day']}</b> a las {CHANGE_DAY[current_day]['up']['time']}\n"
+        message += f"<b>Retrocesos</b> vigentes desde el <b>{CHANGE_DAY[current_day]['down']['day']}</b> a las {CHANGE_DAY[current_day]['down']['time']}\n"
+    try_msg(bot,
+            chat_id=channel_id,
+            parse_mode="HTML",
+            text=message)
+    message = ""
 
     for region in changes:
-        message += f"<b>{region['nombre']}</b>\n\n"
+        message += f"<b>— {region['nombre']} —</b>\n"
+        message += "\n"
         for comuna in region["comunas"]:
             prev = int(comuna["prev"])
             curr = int(comuna["paso"])
             action = "Avanza" if curr > prev else "Retrocede"
-            message += f"<b>{comuna['nombre']}</b>: {action}\n<del><i>Paso {prev} {PASOS_NAMES[prev]}</i></del>\n{PASOS_EMOJIS[curr]} Paso {curr} {PASOS_NAMES[curr]}\n"
+            message += f"<b>{comuna['nombre']}</b>: {action}\n"
+            message += f"  {PASOS_EMOJIS[curr]} Paso {curr} {PASOS_NAMES[curr]}\n"
         message += "\n"
 
-    current_day = int(datetime.now().strftime('%w'))
-    if current_day in CHANGE_DAY:
-        message += f"<b>Avances</b> vigentes desde el <b>{CHANGE_DAY[current_day]['up']['day']}</b> a las {CHANGE_DAY[current_day]['up']['time']}\n"
-        message += f"<b>Retrocesos</b> vigentes desde el <b>{CHANGE_DAY[current_day]['down']['day']}</b> a las {CHANGE_DAY[current_day]['down']['time']}\n"
     send_long_message(bot,
                       chat_id=channel_id,
                       parse_mode="HTML",
-                      text=message)
+                      text=message,
+                      max_length=1800,
+                      slice_str="\n<b>—")
 
 
-def get_steps_counts():
+def get_steps_counts(current = True):
     counts = [0, 0, 0, 0]
-    for region in data.current_data["regiones"]:
+    for region in (data.current_data["regiones"] if current else data.new_data["regiones"]):
         for comuna in region["comunas"]:
             counts[int(comuna["paso"]) - 1] += 1
     
